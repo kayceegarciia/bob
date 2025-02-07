f2f5797a9195c37dd58655abb5172d5c61f62fd5
Kaycee Garcia <kayceegarcia2004@gmail.com>
2025-02-07 11:24:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const slider = document.getElementById('slider');
 const image = document.getElementById('image');
+image.style.opacity = 0;
+
+slider.value = 0;
 
 slider.addEventListener('input', () => {
     const value = slider.value;
     image.style.opacity = value / 100;
 });
+
